4561dc2b64d966e739f4605ead9b96fd0f1e1132
iqbal07 <iqbalhossen0711@gmail.com>
2026-03-11 23:11:52 +0600

    feat: show all bottles data

--- src/components/bottle/Bottle.jsx
+++ src/components/bottle/Bottle.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./bottle.css";
 
-function Bottle() {
+function Bottle({ bottle }) {
+  const { name, img, seller, price, stock, ratings } = bottle;
+
   return (
-    <div>Bottle</div>
-  )
+    <div className="bottle-card">
+      <img src={img} alt={name} />
+
+      <div className="bottle-info">
+        <h3>{name}</h3>
+
+        {/* এই ডিভটি ডাটাগুলোকে পাশাপাশি সাজাতে সাহায্য করবে */}
+        <div className="info-grid">
+          <p>
+            <strong>Seller:</strong> {seller}
+          </p>
+          <p>
+            <strong>Price:</strong> ${price}
+          </p>
+          <p>
+            <strong>Stock:</strong> {stock} left
+          </p>
+          <p>
+            <strong>Rating:</strong> {ratings} ★
+          </p>
+        </div>
+      </div>
+
+      <button className="success-btn">Buy Now</button>
+    </div>
+  );
 }
 
-export default Bottle
+export default Bottle;
--- src/components/bottles/Bottles.jsx
+++ src/components/bottles/Bottles.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { use } from "react";
+import Bottle from "../bottle/Bottle";
+import "./bottles.css"; // CSS ইম্পোর্ট করতে ভুলবেন না
 
-function Bottles() {
+function Bottles({ bottlesPromise }) {
+  const bottlesData = use(bottlesPromise);
+
   return (
-    <div>Bottles</div>
-  )
+    <div className="bottles-container">
+      {bottlesData.map((bottle) => (
+        <Bottle key={bottle.id} bottle={bottle}></Bottle>
+      ))}
+    </div>
+  );
 }
 
-export default Bottles
+export default Bottles;
